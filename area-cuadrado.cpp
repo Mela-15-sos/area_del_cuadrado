6f96dbf9b5efbeb7922079a057b510dcae628fea
Mela-15-sos <sossmela037@gmail.com>
2025-04-28 08:59:18 -0600

    Add files via upload

---
+++ area-cuadrado.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+int main(){
+int lado, area;
+std::cout<< "ingresa la longitud del lado del cuadrado:";
+std::cin>> lado;
+area=lado*lado;
+std::cout<<"el area del cuadrado es:"<<area;
+return 0;
+}
